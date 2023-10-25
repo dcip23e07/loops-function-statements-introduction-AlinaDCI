5b304998927d46fb1c2c8769b93785acaab666a9
Alina <alina.zaslavska@dci-student.org>
2023-10-25 16:10:41 +0200

    ATM code

---
+++ statements.py
@@ -0,0 +1,33 @@
+#input data
+current_bal = 500
+min_withdrawal = 20
+max_withdrawal = 1000
+withdrawalamount = 0
+
+#Exit function
+def exit():
+    print ("Thank you for choosing our bank! Have a good day!")
+def transaction(current_bal):
+    while True:
+        cashamount:int = int(input("Enter cash amount to be withdrawn: "))
+        if cashamount <= current_bal and cashamount <= max_withdrawal and cashamount >= min_withdrawal:
+            current_bal -= cashamount
+            print("Transaction successful. Remaining balance:", current_bal )
+            break
+        elif cashamount > max_withdrawal or cashamount < min_withdrawal:
+            print("This transaction cannot be processed. Withdrawal limits are violated")
+            break
+        elif cashamount > current_bal:
+            print("Transaction failed. Insufficient funds")
+            break
+
+        else:
+            break
+    return current_bal
+
+user_input=input("Do you want to withdraw cash? Enter y or n:")
+if user_input=="y":
+    result = transaction(current_bal)
+else:
+    exit()
+
